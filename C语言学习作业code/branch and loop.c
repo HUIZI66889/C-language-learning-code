226516b30e9454358763e720bd0c3e6edd44b6b3
HUIZI66889 <121297043+HUIZI66889@users.noreply.github.com>
2023-02-18 14:25:14 +0800

    修改1

--- C语言学习作业code/branch and loop.c
+++ C语言学习作业code/branch and loop.c
@@ -1,19 +1,79 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 
-//给定两个数，求这两个数的最大公约数
-//测试
-int main()
-{
-	int a = 0;
-	int b = 0;
-	scanf("%d%d", &a, &b);
+////打印素数：写一段代码，打印100-200之间的素数
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		//判断i是否为素数
+//		//素数判断规则：1.试除法：素数只能被1和它本身整除
+//		//产生2到i-1的数字
+//		int j = 0;
+//		for (j = 2; j < i; j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (j == i)
+//		{
+//			count++;
+//			printf("%d ", i);
+//		}
+//	}
+//	printf("\ncount=%d\n", count);
+//	return 0;
+//}
+
+
+////打印1000到2000年之间的闰年
+//
+//int main()
+//{
+//	int year = 0;
+//	int count = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		//判断是否是闰年。1.能被4整除并且不能被100整除是闰年
+//		//2.能被400整除是闰年
+//		if (year%4 == 0 && year%100 != 0)
+//		{
+//			printf("%d ", year);
+//			count++;
+//		}
+//		else if (year % 400 == 0)
+//		{
+//			printf("%d ", year);
+//			count++;
+//		}
+//	}
+//	printf("\ncount = %d\n", count);
+//	return 0;
+//}
 
 
 
-	return 0;
-}
-
+////给定两个数，求这两个数的最大公约数
+//
+//int main()
+//{
+//	int m = 24;
+//	int n = 18;
+//	int r = 0;
+//
+//	while (m%n)
+//	{
+//		r = m%n;
+//		m = n;
+//		n = r;
+//	}
+//	printf("%d\n", n);
+//	return 0;
+//}
 
 
 ////写一段代码打印1-100之间所有3的倍数的数字
