65e4c8c7a8c14a31293952ff8adb1cfd9e9907af
HUIZI66889 <121297043+HUIZI66889@users.noreply.github.com>
2023-02-19 11:40:20 +0800

    12

--- C语言学习作业code/课程代码.c
+++ C语言学习作业code/课程代码.c
@@ -1,29 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
-//go to 语句
-//一个关机程序
-#include<string.h>
-#include<stdlib.h>
-int main()
-{
-	char input[20] = { 0 };
-	//shutdown -s -t 60 是一个关机命令
-	//system()  是执行系统命令的，括号里面的要用双引号引起来
-	system("shutdown -s -t 120");
-again:
-	printf("请注意你的电脑在2分钟内关机，如果输入：我是猪，就取消关机\n 请输入>:");
-	scanf("%s", input);
-	if (strcmp(input, "我是猪") == 0)
-	{
-		system("shutdown -a");
-	}
-	else
-	{
-		goto again;
-	}
-	return 0;
-}
+////go to 语句
+////一个关机程序
+//#include<string.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	char input[20] = { 0 };
+//	//shutdown -s -t 60 是一个关机命令
+//	//system()  是执行系统命令的，括号里面的要用双引号引起来
+//	system("shutdown -s -t 30");
+//again:
+//	printf("请注意你的电脑在30s内关机，如果输入：我是猪，就取消关机\n 请输入>:");
+//	scanf("%s", input);
+//	if (strcmp(input, "我是猪") == 0)
+//	{
+//		system("shutdown -a");
+//	}
+//	else
+//	{
+//		goto again;
+//	}
+//	return 0;
+//}
 
 
 
