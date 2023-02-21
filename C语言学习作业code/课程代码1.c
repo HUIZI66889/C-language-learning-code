883a6dfa75a7b803e3181909e4e80549f4807ce3
HUIZI66889 <121297043+HUIZI66889@users.noreply.github.com>
2023-02-21 09:55:05 +0800

    1

--- C语言学习作业code/课程代码1.c
+++ C语言学习作业code/课程代码1.c
@@ -1,24 +1,66 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
-//B站P10
-//写一个函数，实现整形有序数组的二分查找
-//如果找到了返回下标，找不到返回-1
-int main()
-{
-	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-	int k = 7;
-	int ret = binary_search(arr, k);
-	if (ret == -1)
-	{
-		printf("找不到指定的数子\n");
-	}
-	else
-	{
-		printf("找到了，下标是：%d\n", ret);
-	}
-	return 0;
-}
+////B站P10
+////写一个函数，每调用这个函数，就会将num的值+1
+//void Add(int* p)
+//{
+//	(*p)++;
+//}
+//int main()
+//{
+//	int num = 0;
+//	Add(&num); //要在Add函数中操作改变num的值，要传地址进去
+//	printf("num = %d\n",num); //1
+//	Add(&num);
+//	printf("num = %d\n", num); //2
+//	Add(&num);
+//	printf("num = %d\n", num); //3
+//	return 0;
+//}
+
+
+////B站P10
+////写一个函数，实现整形有序数组的二分查找
+////如果找到了返回下标，找不到返回-1
+//int binary_search(int arr[], int k, int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else if(arr[mid]>k)
+//		{
+//			right = mid - 1;
+//		}
+//		else
+//		{
+//			return mid;
+//		}
+//	}
+//	return -1;
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int ret = binary_search(arr, k, sz);
+//	if (ret == -1)
+//	{
+//		printf("找不到指定的数子\n");
+//	}
+//	else
+//	{
+//		printf("找到了，下标是：%d\n", ret);
+//	}
+//	return 0;
+//}
 
 ////B站P10
 ////写一个函数路判断一年是不是闰年
