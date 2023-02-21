73fb7407f8418b0967490bad740935d974488219
HUIZI66889 <121297043+HUIZI66889@users.noreply.github.com>
2023-02-21 17:41:53 +0800

    211

--- C语言学习作业code/课程代码2.c
+++ C语言学习作业code/课程代码2.c
@@ -1,1 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//B站P11
+//递归
+//编写函数不允许创建临时变量，求字符串的长度
+
+
+////B站P11
+////递归
+////接收一个整型值（无符号），按照顺序打印它的每一位。例如：输入：1234，输出1 2 3 4.
+//void print(int n) //1234
+//{
+//	if (n > 9)
+//	{
+//		print(n / 10);
+//	}
+//	printf("%d", n % 10);
+//}
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%d ", &num);
+//	print(num);
+//	return 0;
+//}
+
+
+////B站P11
+////最简单的递归
+//int main()
+//{
+//	printf("hehe\n");
+//	main();
+//	return 0;
+//} //打印结果是hehe的死循环
