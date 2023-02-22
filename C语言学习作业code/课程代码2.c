31c2d3832917e9c0340a59192b28ef58906d2f43
HUIZI66889 <121297043+HUIZI66889@users.noreply.github.com>
2023-02-22 09:42:31 +0800

    12

--- C语言学习作业code/课程代码2.c
+++ C语言学习作业code/课程代码2.c
@@ -1,6 +1,114 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
+
+//B站P12
+
+
+
+
+
+
+////B站P12
+////求第n个斐波那契数（不考虑溢出）---循环
+//int Fib(int n)
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n > 2)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//	//TDD--测试驱动开发（先想函数怎么用，再想如何实现）
+//	ret = Fib(n);
+//	printf("ret = %d\n", ret);
+//	return 0;
+//}
+
+
+////B站P12
+////求第n个斐波那契数（不考虑溢出）---前两个数之和等于第三个数
+//int count = 0;
+//int Fib(int n)
+//{ 
+//	//if (n == 3) //计算第3个斐波那契数的计算次数
+//	//{
+//	//	count++;
+//	//}
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return Fib(n - 1) + Fib(n - 2);
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//	//TDD--测试驱动开发（先想函数怎么用，再想如何实现）
+//	ret = Fib(n);
+//	printf("ret = %d\n", ret);
+//	printf("count = %d\n", count);
+//	return 0;
+//}
+
+
+////B站P12
+////迭代
+////求n的阶乘（不考虑溢出）---递归的方式
+//int Fac2(int n)
+//{
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n * Fac2(n - 1);
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//	ret = Fac2(n); //递归的方式
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+////B站P12
+////迭代
+////求n的阶乘（不考虑溢出）---循环的方式
+//int Fac1(int n)
+//{
+//	int i = 0;
+//	int ret = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//	ret = Fac1(n); //循环的方式
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
 ////B站P11
 ////递归
 ////编写函数不允许创建临时变量（下例中不用count），求字符串的长度
