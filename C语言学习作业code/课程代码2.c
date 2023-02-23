6a7ac20ad9cb00318aea9ba8faf64b7b6790e7f3
HUIZI66889 <121297043+HUIZI66889@users.noreply.github.com>
2023-02-23 10:36:35 +0800

    32

--- C语言学习作业code/课程代码2.c
+++ C语言学习作业code/课程代码2.c
@@ -3,12 +3,13 @@
 
 //B站P13
 //实现一个冒泡函数，将整形数组进行排序
-void bubble_sort(int arr[],int sz)
+void bubble_sort(int arr[], int sz)
 {
 	//确定冒泡排序的趟数
 	int i = 0;
 	for (i = 0; i < sz - 1; i++)
 	{
+		int flag = 1; //假设这一趟要排序的数据已经有序
 		//每一趟冒泡排序
 		int j = 0;
 		for (j = 0; j < sz - 1 - i; j++) //sz-1-i是每一趟比较的对儿数
@@ -18,10 +19,14 @@ void bubble_sort(int arr[],int sz)
 				int tmp = arr[j];
 				arr[j] = arr[j + 1];
 				arr[j + 1] = tmp;
+				flag = 0; //本趟排序的数据其实不完全有序
 			}
 		}
+		if (flag == 1)
+		{
+			break; //跳出int i =0下面的for循环
+		}
 	}
-
 }
 int main()
 {
