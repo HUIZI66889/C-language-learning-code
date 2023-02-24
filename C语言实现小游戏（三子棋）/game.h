805448dc71ee412f85710eeb617637b4f72e12bd
HUIZI66889 <121297043+HUIZI66889@users.noreply.github.com>
2023-02-24 14:58:46 +0800

    4

---
+++ C语言实现小游戏（三子棋）/game.h
@@ -0,0 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#define ROW 3
+#define COL 3
+
--- C语言实现小游戏（三子棋）/test.c
+++ C语言实现小游戏（三子棋）/test.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include<stdio.h>
+#include "game.h"
 
 //B站P15
 //测试三子棋
@@ -9,6 +10,14 @@ void menu()
 	printf("****************************************\n");
 	printf("************1.play   0.exit*************\n");
 	printf("****************************************\n");
+}
+
+//游戏的算法实现
+void game()
+{
+	//数组-存放走出的棋盘信息
+	char board[ROW][COL] = { 0 };
+
 }
 
 void test()
@@ -22,6 +31,7 @@ void test()
 		switch (input)
 		{
 		case 1:
+			game();
 			printf("三子棋\n");
 			break;
 		case 0:
