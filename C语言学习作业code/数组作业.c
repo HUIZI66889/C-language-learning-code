98254b8882ccc8bba78eb267b0948825a3878db8
HUIZI66889 <121297043+HUIZI66889@users.noreply.github.com>
2023-03-04 11:03:49 +0800

    43

--- C语言学习作业code/数组作业.c
+++ C语言学习作业code/数组作业.c
@@ -1,1 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//B站P23
+//创建一个整型数组，完成对数组的操作
+//1.实现函数init初始化数组为全0
+//2.是实现print()，打印数组的每个元素
+//3.实现reverse()，函数完成数组元素的逆置
+//要求：自己设计以上函数的参数，返回值
+
+int main()
+{
+
+	return 0;
+}
