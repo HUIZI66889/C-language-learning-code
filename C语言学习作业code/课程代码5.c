6c53dcf6bacdedf28d56a32b85fbafc20e93169a
HUIZI66889 <121297043+HUIZI66889@users.noreply.github.com>
2023-03-04 11:00:08 +0800

    43

--- C语言学习作业code/课程代码5.c
+++ C语言学习作业code/课程代码5.c
@@ -1,30 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
-
-
-//B站P22
-//指针数组
-int main()
-{
-	int a = 10;
-	int b = 20;
-	int c = 30;
-	//int* pa = &a;
-	//int* pb = &b;
-	//int* pc = &c;
-	//数组存放整型指针
-	//整型数组-存放整型
-	//字符数组-存放字符
-	//指针数组-存放指针
-	int* arr2[3] = {&a,&b,&c};//指针数组
-	int i = 0;
-	for (i = 0; i < 3; i++)
-	{
-		printf("%d ", * (arr2[i]));
-	}
-	return 0;
-}
+////B站P22
+////指针数组
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//	//int* pa = &a;
+//	//int* pb = &b;
+//	//int* pc = &c;
+//	//数组存放整型指针
+//	//整型数组-存放整型
+//	//字符数组-存放字符
+//	//指针数组-存放指针
+//	int* arr2[3] = {&a,&b,&c};//指针数组
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("%d ", * (arr2[i]));
+//	}
+//	return 0;
+//}
 
 
 ////B站P22
