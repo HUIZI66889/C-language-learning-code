08e9042fc67eb96ee31c8de2d8d43caeae53f576
HUIZI66889 <121297043+HUIZI66889@users.noreply.github.com>
2023-03-05 18:53:36 +0800

    43

--- C语言学习作业code/数组、函数、操作符作业.c
+++ C语言学习作业code/数组、函数、操作符作业.c
@@ -2,24 +2,24 @@
 #include<stdio.h>
 
 
-//B站P24
-//使用指针打印数组内容
-//写一个函数打印arr数组的内容，不使用数组下标，使用指针，arr是一个整型一维数组
-void print(int* p, int sz)
-{
-	int i = 0;
-	for (i = 0; i < sz; i++)
-	{
-		printf("%d ", * (p + i));
-	}
-}
-int main()
-{
-	int arr[] = { 1,2,3,4,5,6,7,8,9 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	print(arr, sz);
-	return 0;
-}
+////B站P24
+////使用指针打印数组内容
+////写一个函数打印arr数组的内容，不使用数组下标，使用指针，arr是一个整型一维数组
+//void print(int* p, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", * (p + i));
+//	}
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(arr, sz);
+//	return 0;
+//}
 
 
 ////B站P24
