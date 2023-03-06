ad74ae1b75b34e10ce6ca3467c7ac8e26ab2f866
HUIZI66889 <121297043+HUIZI66889@users.noreply.github.com>
2023-03-06 16:10:56 +0800

    6

--- C语言学习作业code/数组、函数、操作符作业.c
+++ C语言学习作业code/数组、函数、操作符作业.c
@@ -1,16 +1,124 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
-//B站P24
-//字符串逆序（递归实现）
-//编写一个函数reverse_string(char* string)(递归实现)
-//实现：将参数字符串中的字符反向排列
-//要求：不能使用C函数库中的字符串操作函数
-int main()
-{
 
-	return 0;
-}
+////B站P24
+////递归实现n的k次方
+////编写一个函数实现n的k次方，使用递归实现
+//double Pow(int n, int k)
+//{
+//	//n^k=n*n^(k-1)
+//	if (k < 0)
+//		return (1.0 / (Pow(n, -k)));
+//	else if (k == 0)
+//		return 1;
+//	else
+//		return n * Pow(n, k - 1);
+//}
+//int main()
+//{
+//	int n = 0;
+//	int k = 0;
+//	scanf("%d%d", &n, &k);
+//	double ret = Pow(n,k);
+//	printf("ret = %lf\n", ret);
+//	return 0;
+//}
+
+
+////B站P24
+////计算一个数的每位之和（递归实现）
+////写一个递归函数DigitSun(n),输入一个非负整数，返回组成它的数字之和
+////例如：调用DigitSum(1729),则应该返回1+7+2+9，它的和是19
+////输入：1729，输出：19
+//int DigitSum(unsigned int num)
+//{
+//	if (num > 9)
+//	{
+//		return DigitSum(num / 10) + num % 10;
+//	}
+//	else
+//	{
+//		return num;
+//	}
+//}
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%d", &num);
+//	int ret = DigitSum(num);
+//	printf("ret =%d\n", ret);
+//	return 0;
+//}
+
+
+////B站P24
+////字符串逆序（下题使用递归实现）
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//void reverse_string(char* arr)
+//{
+//	char tmp = arr[0];
+//	int len = my_strlen(arr);
+//	arr[0] = arr[len - 1];
+//	arr[len - 1] = '\0';
+//	if(my_strlen(arr+1)>=2)
+//		reverse_string(arr + 1);//数组名是首元素地址，+1是第二个元素的地址
+//	arr[len - 1] = tmp;
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	reverse_string(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+////B站P24
+////字符串逆序（循环实现）
+////编写一个函数reverse_string(char* string)(循环实现)
+////实现：将参数字符串中的字符反向排列，不是简单的逆序打印
+////要求：不能使用C函数库中的字符串操作函数
+////#include<string.h>
+//int my_strlen(char*str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//void reverse_string(char* arr)
+//{
+//	int left = 0;
+//	int right = my_strlen(arr)-1;
+//	while (left < right)
+//	{
+//		int tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";//字符串内容逆序，fedcba
+//	reverse_string(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
 
 
 ////B站P24
