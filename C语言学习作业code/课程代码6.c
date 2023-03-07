3b7079ab34df2265c2312957deb2add33d0dda92
HUIZI66889 <121297043+HUIZI66889@users.noreply.github.com>
2023-03-07 08:12:26 +0800

    6

--- C语言学习作业code/课程代码6.c
+++ C语言学习作业code/课程代码6.c
@@ -1,21 +1,48 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
+
+
 //B站P26
-//结构体的声明（定义一个学生类型）
-//描述一个学生-需要一些数据，放在大括号里，名字、年龄、电话、性别
-//struct是结构体关键字；Stu是结构体标签；struct Stu是结构体类型
-struct Stu
-{
-	//成员变量
-	char name[20];
-	short age;
-	char tele[12];
-	char sex[5];
-};
+//
 int main()
 {
-	//变量的创建
-	struct Stu s;//因为struct Stu是结构体类型，结构体类型用来创建变量的，故创建一个s变量
+
 	return 0;
 }
+
+
+////B站P26
+////结构体的声明（定义一个学生类型）
+////描述一个学生-需要一些数据，放在大括号里，名字、年龄、电话、性别
+////struct是结构体关键字；Stu是结构体标签；struct Stu是结构体类型
+//struct Stu
+//{
+//	//成员变量
+//	char name[20];
+//	short age;
+//	char tele[12];
+//	char sex[5];
+//}; s1, s2, s3;//s1, s2, s3是3个全局的结构体变量
+//int main()
+//{
+//	//变量的创建
+//	struct Stu s;//因为struct Stu是结构体类型，结构体类型用来创建变量的，故创建一个s变量，该变量为局部变量
+//	return 0;
+//}
+
+////第2种定义类型的方式
+//typedef struct Stu
+//{
+//	//成员变量
+//	char name[20];
+//	short age;
+//	char tele[12];
+//	char sex[5];
+//}Stu;
+//int main()
+//{
+//	//变量的创建
+//	Stu s2;//Stu也可以创建s2了
+//	return 0;
+//}
