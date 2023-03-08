74f4bb723b357180ae2700e339336101f65524f5
HUIZI66889 <121297043+HUIZI66889@users.noreply.github.com>
2023-03-08 22:22:59 +0800

    65

--- C语言学习code(高阶)/课程代码2.c
+++ C语言学习code(高阶)/课程代码2.c
@@ -1,50 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
-//B站P33
-//
-int main()
-{
-
-	return  0;
-}
-
-
 ////B站P33
-////数组指针用在二维数组更方便
-//void print1(int arr[3][5], int x, int y)
-//{
-//	int i = 0;
-//	int j = 0;
-//	for (i = 0; i < x; i++)
-//	{
-//		for (j = 0; j < y; j++)
-//		{
-//			printf("%d ", arr[i][j]);
-//		}
-//		printf("\n");
-//	}
-//}
-//void print2(int(*p)[5], int x, int y)
-//{
-//	int i = 0;
-//	int j = 0;
-//	for (i = 0; i < x; i++)
-//	{
-//		for (j = 0; j < y; j++)
-//		{
-//			printf("%d ", p[i][j]);
-//		}
-//		printf("\n");
-//	}
-//}
+////
 //int main()
 //{
-//	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7} };
-//	print1(arr, 3, 5);//arr是数组名，是首元素地址
-//	print2(arr, 3, 5);
-//	return 0;
+//
+//	return  0;
 //}
+
+
+//B站P33
+//数组指针用在二维数组更方便
+//参数是数组的形式
+void print1(int arr[3][5], int x, int y)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < x; i++)
+	{
+		for (j = 0; j < y; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+}
+//参数是指针的形式
+void print2(int(*p)[5], int x, int y)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < x; i++)
+	{
+		for (j = 0; j < y; j++)
+		{
+			printf("%d ", *(*(p+i)+j));//*（p+i）是找到i行的数据解引用，+j是找到i行j列的元素解引用（找到某个元素）
+		}//或者写成：(*(p+i))[j]
+		printf("\n");
+	}
+}
+int main()
+{
+	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7} };
+	print1(arr, 3, 5);//arr是数组名，是首元素地址
+	print2(arr, 3, 5);
+	return 0;
+}
 
 
 ////B站P33
