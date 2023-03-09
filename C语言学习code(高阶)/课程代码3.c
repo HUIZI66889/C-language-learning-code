0b74c096a85ede71872c1e4379205d192a340a04
HUIZI66889 <121297043+HUIZI66889@users.noreply.github.com>
2023-03-09 11:19:28 +0800

    43

--- C语言学习code(高阶)/课程代码3.c
+++ C语言学习code(高阶)/课程代码3.c
@@ -1,13 +1,54 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
-//B站P34
-//
-int main()
-{
 
-	return 0;
-}
+////B站P34
+////函数指针数组
+//int main()
+//{
+//	char* my_strcpy(char* dest, const char* src);
+//	
+//	//写一个函数指针pf，能够指向my_strcpy
+//	char* (*pf)(char*, const char*);
+//	
+//	//写一个函数指针数组pfarr，能够存放4个my_strcpy函数的地址
+//	char* (*pfarr[4])(char*, const char*);
+//	return 0;
+//}
+
+
+////B站P34
+////函数指针数组
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int Mul(int x, int y)
+//{
+//	return x * y;
+//}
+//int Div(int x, int y)
+//{
+//	return x / y;
+//}
+//int main()
+//{
+//	//指针数组
+//	int* arr[5];
+//	//需要一个数组，这个数组可以存放4个函数的地址
+//	int (*pa)(int, int) = Add;//pa可以存Sub,Mul,Div的地址，因为它们的参数和返回类型是一样的
+//	int (*parr[4])(int, int) = {Add,Sub,Mul,Div };//函数指针的数组
+//	int i = 0;
+//	for (i = 0; i < 4; i++)
+//	{
+//		printf("%d\n",parr[i](2, 3));//i是1调用加法，i是2调用减法...；5，-1，6，0
+//	}
+//	return 0;
+//}
 
 
 ////B站P34
