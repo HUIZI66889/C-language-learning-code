ec70ea33c37ebd725ce3099138da673f073d6d19
HUIZI66889 <121297043+HUIZI66889@users.noreply.github.com>
2023-03-12 20:20:21 +0800

    3

--- C语言学习code(高阶)/课程代码4.c
+++ C语言学习code(高阶)/课程代码4.c
@@ -2,8 +2,22 @@
 #include<stdio.h>
 
 //B站P40
-//
+//实现strcpy
+#include<assert.h>
+my_strcpy(char* dest, char* src)
+{
+	assert(dest != NULL);
+	assert(dest != NULL);
+	while (*dest++ = *src++)
+	{
+		;
+	}
+}
 int main()
 {
+	char arr1[] = "abcdefghi";
+	char arr2[] = "bit";
+	my_strcpy(arr1, arr2);
+	printf("%s\n", arr1);
 	return 0;
 }
