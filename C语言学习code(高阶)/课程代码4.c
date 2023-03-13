d6bd563c724174ba8350afc4cbe95a046ec136da
HUIZI66889 <121297043+HUIZI66889@users.noreply.github.com>
2023-03-13 10:47:57 +0800

    0

--- C语言学习code(高阶)/课程代码4.c
+++ C语言学习code(高阶)/课程代码4.c
@@ -2,7 +2,35 @@
 #include<stdio.h>
 
 
-
+////B站P49
+////实现strcmp
+//#include<assert.h>
+//int my_strcmp(const char* str1, const char* str2)
+//{
+//	assert(str1 && str2);
+//	//比较
+//	while (*str1 == *str2)
+//	{
+//		if (*str1 == '\0')
+//		{
+//			return 0;
+//		}
+//		str1++;
+//		str2++;
+//	}
+//	if (*str1 > *str2)
+//		return 1;//大于
+//	else
+//		return -1;
+//}
+//int main()
+//{
+//	char* p1 = "abcdef";
+//	char* p2 = "abcdef";
+//	int ret = my_strcmp(p1, p2);
+//	printf("ret = %d\n", ret);
+//	return 0;
+//}
 
 
 ////B站P40
