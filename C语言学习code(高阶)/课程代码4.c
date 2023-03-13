4331b53623f1eacbfcf5d0930fe4d722879880fa
HUIZI66889 <121297043+HUIZI66889@users.noreply.github.com>
2023-03-13 10:03:32 +0800

    666

--- C语言学习code(高阶)/课程代码4.c
+++ C语言学习code(高阶)/课程代码4.c
@@ -1,27 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
-//B站P40
-//实现strcpy
-#include<assert.h>
-char* my_strcpy(char* dest, const char* src)
-{
-	assert(dest != NULL);
-	assert(dest != NULL);
-	char* ret = dest;
-	//拷贝src指向的字符串到dest指向的空间，包含\0
-	while (*dest++ = *src++)
-	{
-		;
-	}
-	//返回目的空间的起始地址
-	return ret;
-}
-int main()
-{
-	char arr1[] = "abcdefghi";
-	char arr2[] = "bit";
-	my_strcpy(arr1, arr2);
-	printf("%s\n", arr1);
-	return 0;
-}
+
+
+
+
+////B站P40
+////实现strcat
+//int main()
+//{
+//	char arr1[30] = "hello";
+//	char arr2[] = "world";
+//	strcat(arr1, arr2);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+
+
+////B站P40
+////实现strcpy
+//#include<assert.h>
+//char* my_strcpy(char* dest, const char* src)
+//{
+//	assert(dest != NULL);
+//	assert(dest != NULL);
+//	char* ret = dest;
+//	//拷贝src指向的字符串到dest指向的空间，包含\0
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	//返回目的空间的起始地址
+//	return ret;
+//}
+//int main()
+//{
+//	char arr1[] = "abcdefghi";
+//	char arr2[] = "bit";
+//	my_strcpy(arr1, arr2);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
